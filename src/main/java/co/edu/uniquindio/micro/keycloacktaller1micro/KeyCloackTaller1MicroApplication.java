ea5306b35e76f7bbdf62bbedd281a2bdf36b96f5
AndresZunigaZ2005 <andresf.zunigaz@uqvirtual.edu.co>
2025-08-20 10:04:45 -0500

    First Commit

---
+++ src/main/java/co/edu/uniquindio/micro/keycloacktaller1micro/KeyCloackTaller1MicroApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.micro.keycloacktaller1micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyCloackTaller1MicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyCloackTaller1MicroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uniquindio/micro/keycloacktaller1micro/KeyCloackTaller1MicroApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.micro.keycloacktaller1micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyCloackTaller1MicroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
