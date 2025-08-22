2fb303dd56450fac663f036103fe22e70ba9c5a1
MiguelA05 <126737880+MiguelA05@users.noreply.github.com>
2025-08-21 21:49:17 -0500

    Remove unused saludo endpoint from DemoController
    Deleted the /saludo endpoint and related imports from DemoController as it is no longer needed. This simplifies the controller and removes unnecessary code.

--- src/main/java/co/edu/uniquindio/micro/keycloacktaller1micro/controller/DemoController.java
+++ src/main/java/co/edu/uniquindio/micro/keycloacktaller1micro/controller/DemoController.java
@@ -1,8 +1,6 @@
 package co.edu.uniquindio.micro.keycloacktaller1micro.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
 
@@ -19,9 +17,5 @@ public class DemoController {
         return "Hola " + principal.getName() + ", accediste con un token válido 🔑";
     }
 
-    @CrossOrigin(origins = "http://localhost:8083")
-    @GetMapping("/saludo")
-    public String saludo(@RequestParam String nombre) {
-        return "Hola, " + nombre + "!";
-    }
+
 }
